20f8c80c07d49941c0465e8edbd17324b0630ebe
sabiryosf <youssef2000sabir@gmail.com>
2023-07-14 16:31:46 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,29 +2,30 @@
 
 /**
  * create_file - create file
- * @file_name: blblbl
+ * @filename: blblbl
  * @text_content: blblbl
  * Return: if fails --1, other -1
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len =0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
-		return -1;
+		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++);
+	for (len = 0; text_content[len]; len++);
 	}
+
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
-		return -1;
+		return (-1);
 
 	close(fd);
 
-	return 1;
+	return (1);
 }
